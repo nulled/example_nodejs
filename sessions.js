83ed736e1fb3f48ddf365559b6e6d3c26e363217
nulled <elitescripts2000@yahoo.com>
2014-04-09 02:04:42 -0700

    session.js update

--- sessions.js
+++ sessions.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var RedisStore = require('connect-redis')(session);
 var storeRedis = new RedisStore({url: 'redis://127.0.0.1:6379/dump', ttl: (3600 * 1), db: 0, prefix: ''});
 var env = (function(){
@@ -23,17 +25,20 @@ var db = MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db)
 var collection = db.collection('test_insert');
 */
 
-app.use(express.cookieParser());
+var sessionCookieParams = { domain: '10.10.10.25', path: '/', httpOnly: true, secure: false };
+
+//app.use(bodyParser());
+app.use(cookieParser());
 app.use(session({store: storeRedis,
                  secret: env.get('SECRETKEY'),
                  key: 'sessionID',
-                 cookie: { domain: 'localhost', path: '/', httpOnly: true, secure: false }
+                 cookie: sessionCookieParams
 }));
 
 app.get('/', function(req, res) {
     var output = '';
-    for (var prop in req) {
-      output += prop + ': ' + req[prop] + "\n";
+    for (var prop in res) {
+        output += prop + ': ' + req[prop] + "\n";
     }
     res.end(output);
 });
@@ -45,32 +50,30 @@ app.get('/home', function(req, res) {
 app.get('/awesome', function(req, res) {
     if (req.session.lastPage) {
         res.write('Last page was: ' + req.session.lastPage + '. ');
-        res.write('Lasty page was: ' + req.session.lastPage + '. ');
     }
     req.session.lastPage = '/awesome';
-    res.end("You're Awesome.");
+    res.end('You\'re Awesome.');
 });
 
 app.get('/radical', function(req, res) {
     if (req.session.lastPage) {
         res.write('Last page was: ' + req.session.lastPage + '. ');
     }
-
     req.session.lastPage = '/radical';
     res.end('What a radical visit!');
 });
 
-app.get('/tubular', function(req, res) {
-    res.end('Are you a surfer?');
+// http://10.10.10.25:3000/tubular/321 --> req.params.id
+// http://10.10.10.25:3000/tubular/321/?id=123 --> req.params.id=321 and req.query.id=123
+app.get('/tubular/:id', function(req, res) {
+    res.end(req.params.id + ' Are you a surfer? ' + req.query.id);
 });
 
 app.get('/logout', function (req, res) {
-
     req.session.destroy(function (err) {
-      res.clearCookie(req.sessionID, { domain: 'localhost', path: '/', httpOnly: true, secure: false });
+      res.clearCookie(req.sessionID, sessionCookieParams);
       res.redirect('home');
     });
-
 });
 
 app.listen(3000);
